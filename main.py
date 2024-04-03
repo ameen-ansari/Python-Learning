b4ce9ede226f951b5e8a34cc8fca7f7d0572c6be
ZamanTariq <zaman@ogoul.com>
2024-04-03 19:14:41 +0500

    Python Learning Day 3

---
+++ main.py
@@ -0,0 +1,24 @@
+# Veriables in Python
+# userQuestion = 'Please Enter Your Number here '
+
+# How to get input in Python
+# userInput = input(userQuestion.upper())
+
+# Conditions on Python
+# if (int(userInput) > 18):
+#     print('You can drive')
+# else:
+#     print('''You can't drive''')
+
+# For loop in Python
+# for item in range(0 , 20 , 7):
+#     print(item)       
+
+# Functions in Python
+# def sumOfNumbers():
+#     a = input('Give Your Number 1: ')
+#     b = input('Give Your Number 2: ')
+#     c = int(a) + int(b)
+#     print('The total is:',c)
+
+# sumOfNumbers()
